39b63412ea7783f096dc0d0158eaef5c724df161
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-02-23 02:24:42 +0530

    -

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import CarItem from './components/CarItem';
+import StyledButton  from './components/StyledButton';
 export default function App() {
   return (
     <View style={styles.container}>
   
      <CarItem/> 
+
     </View>
   );
 }
---
+++ components/StyledButton/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StyledButton() {
+  return (
+    <View>StyledButton</View>
+  )
+}
